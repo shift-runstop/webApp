70ece0d775289557e8f600215acedf8a829ac2f4
Glitch (spiky-tourmaline-foxglove) <none>
2020-03-04 16:07:22 +0000

    💃💋 Checkpoint ./routes.js:10548466/1 ./models/library.json:10548466/23 ./views/partials/listgenres.hbs:10548466/54 ./.glitch-assets:10548466/1852 ./views/start.hbs:10548466/420 ./models/libraryManager.js:10548466/15

--- routes.js
+++ routes.js
@@ -10,6 +10,7 @@ router.get('/', start.index);
 router.get('/library', library.index);
 router.get('/about', about.index);
 router.get('/genres/:id', genres.index);
+
 router.get('/genres/:id/deleteBook/:bookid', genres.deleteBook);
 router.get('/library/deletegenre/:id', library.deleteGenre);
 module.exports = router;
