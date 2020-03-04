bc4071abdc40957aa6f40a5382a6ad117bca5fae
Glitch (spiky-tourmaline-foxglove) <none>
2020-03-04 14:19:51 +0000

    🚅💴 Checkpoint ./controllers/start.js:10548466/354 ./controllers/about.js:10548466/354 ./package.json:10548466/41 ./routes.js:10548466/127 ./views/start.hbs:10548466/92

--- routes.js
+++ routes.js
@@ -1,25 +1,15 @@
 'use strict';
-
-// import express and initialise router
 const express = require('express');
 const router = express.Router();
-
-// import controllers
 const start = require('./controllers/start.js');
 const dashboard = require('./controllers/library.js');
 const about = require('./controllers/about.js');
 const playlist = require('./controllers/genres.js');
-
-// connect routes to controllers
 router.get('/', start.index);
 router.get('/library', library.index);
 router.get('/about', about.index);
-
 router.get('/genres/:id', genres.index);
-
 router.get('/genres/:id/deleteBook/:bookid', genres.deleteBook);
 router.get('/library/deletegenre/:id', library.deleteGenre);
-
-// export router module
 module.exports = router;
 
