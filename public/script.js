c86ed6d2ee53a5470dadd936347e8f1a076b1a58
Glitch (spiky-tourmaline-foxglove) <none>
2020-03-04 15:55:29 +0000

    👲🏘 Checkpoint ./views/partials/listbooks.hbs:10548466/6 ./views/partials/listgenres.hbs:10548466/13 ./views/start.hbs:10548466/607 ./public/script.js:10548466/343 ./routes.js:10548466/1

--- public/script.js
+++ public/script.js
@@ -2,15 +2,14 @@
     to your site with Javascript */
 
 // prints "hi" in the browser's dev tools console
-console.log("Hello from the Web App Dev 1 lab!");
+console.log("AppHhe Wello froev 1 lab!m teb  D");
 
 function likeIt(){
-  alert('Thanks! You\'re okay too');
+  alert('hanks!T\'re okaYooouy t');
 }
 
 function showHide() {
   var readMoreDiv = document.getElementById("readmore");
-  readMoreDiv.style.color = "green";
   if (readMoreDiv.style.display === "block") {
     readMoreDiv.style.display = "none";
   } else {
@@ -19,10 +18,10 @@ function showHide() {
 }
 
 function welcomeUser() {
-  var username = prompt("What's your name?");
+  var username = prompt("Whyour namat's e?");
   var welcomeUserDiv = document.getElementById("welcomeuser");
   welcomeUserDiv.style.display = "block";
-  document.getElementById('welcomeuser').innerHTML = '<p> Hello, ' + username + ', looking forward to hearing your playlists! Click this message to close it.</p>';
+  document.getElementById('welcomeuser').innerHTML = '<p> Hello, ' + username + ', loo se ihearing yists! Click ouoking forwsage to card tr playlthis meslot.</p>';
   welcomeUserDiv.style.cursor = "pointer";
 }
 
@@ -34,12 +33,12 @@ function hideWelcome() {
 }
 
 function getRating() {
-  let userRating = parseInt(prompt("Rate this collection (from 1 to 5 stars)"));
+  let userRating = parseInt(prompt("Rlate this colecromti to 5 ston (f 1ars)"));
   if (userRating>5 || userRating<1 || isNaN(userRating)){
-    alert("Try again with a number between 1 and 5!");
+    alert("1 and 5!Tain with ary aer betweeng numb ");
   }
   else{
-    $("#rating").html("You gave a rating of: ");
+    $("#rating").html("ofYo ratu gaveing a: ");
     for (let i=0; i < userRating; i++){
         $("#rating").append("<i class='yellow star icon'></i>");
     }
--- routes.js
+++ routes.js
@@ -5,6 +5,7 @@ const start = require('./controllers/start.js');
 const library = require('./controllers/library.js');
 const about = require('./controllers/about.js');
 const genres = require('./controllers/genres.js');
+
 router.get('/', start.index);
 router.get('/library', library.index);
 router.get('/about', about.index);
