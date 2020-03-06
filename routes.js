71fdd005861730aa4eab742a9268c145bae2fecc
Glitch (spiky-tourmaline-foxglove) <none>
2020-03-06 11:56:35 +0000

    Exporting to shift-runstop/webApp ./views/partials/menu.hbs:10548466/26 ./controllers/library.js:10548466/52 ./controllers/genres.js:10548466/24 ./routes.js:10548466/30

--- routes.js
+++ routes.js
@@ -9,11 +9,11 @@ const genres = require('./controllers/genres.js');
 router.get('/', start.index);
 router.get('/library', library.index);
 router.get('/about', about.index);
-router.get('/genres/:id', genres.index);
+router.get('/book/:id', genres.index);
 
-router.get('/genres/:id/deleteBook/:bookid', genres.deleteBook);
+router.get('/book/:id/deleteBook/:bookid', genres.deleteBook);
 router.get('/library/deletegenre/:id', library.deleteGenre);
 
-router.post('/genres/:id/addbook', genres.addBook);
+router.post('/book/:id/addbook', genres.addBook);
 module.exports = router;
 
